eb2ba790c5f1bcfe89c9a0b2015d26890e974d46
oJunHyeon <oh.junhyeon95@gmail.com>
2019-11-17 20:14:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SoptDesignSeminar
+
+- SOPT iOS 5차 디자인 합동세미나 과제입니다.
---
+++ SoptDesign/Sources/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoptDesign
+//
+//  Created by Junhyeon on 2019/11/16.
+//  Copyright © 2019 Junhyeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
